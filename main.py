68ff443d1c581e216da97637f56286605f23b656
LadaLex0 <91010364+LadaLex0@users.noreply.github.com>
2021-09-19 13:55:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+r = float(input('Write the radius of a circle'))
+p = 3.14159
+d = 2 * r
+c = 2 * p * r
+s = p * r ** 2
+print('The diameter is', d)
+print('The circumference is', c)
+print('The area is', s)
+
